fdf19d0c4cbc53a8605f5792dd217e31ed8c52d3
root <root@localhost.localdomain>
2017-08-27 10:07:11 +0000

    Json module skeleton

---
+++ src/__main__.py
@@ -0,0 +1,22 @@
+'''
+    File: __main__.py
+    Description:
+        The entry point for this project.
+    Author: temet 
+'''
+import glob
+import argparse
+
+def grabData(fieglob):
+    filelist = glob.glob(fileglob)
+    return filelist
+
+def main():
+    '''
+        Entry point 
+    '''
+    pass
+
+if __name__ == "__main__":
+    print("main")
+
